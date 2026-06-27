01e81842d4b16ec15786d31a8f3cca720324b9fe
Asmatbangash <bangashasmat2gmail.com>
2026-06-27 12:10:31 +0500

    refactor: countEvenOdd and secondAndThirdLargest implementations

--- ArrayQuestion.js
+++ ArrayQuestion.js
@@ -1,21 +1,21 @@
 // Find maximum and minimum elements in an array
-// function findMaxMin(arr){
-//     if(arr.length === 0){
-//         return null;
-//     }
-//     let max = arr[0];
-//     let min = arr[0];
-//     for(let i=1; i < arr.length; i++){
-//         if(arr[i] > max){
-//             max = arr[i];
-//         }
-//         if(arr[i] < min){
-//             min = arr[i];
-//         }
-//     }
-//     return {max, min};
-// }
-// console.log(findMaxMin([3,1,4,21,5,9]));
+function findMaxMin(arr){
+    if(arr.length === 0){
+        return null;
+    }
+    let max = arr[0];
+    let min = arr[0];
+    for(let i=1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return {max, min};
+}
+console.log(findMaxMin([3,1,4,21,5,9]));
 
 // Reverse an array
 function reverseArray(arr){
@@ -40,3 +40,39 @@ function sumOfArray(arr){
 }
 console.log(sumOfArray([1,2,3,4,5]));
 
+// count the number of even and odd numbers in an array
+function countEvenOdd(arr){
+    let evenCount = 0;
+    let oddCount = 0;
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+    return {evenCount, oddCount};
+}
+console.log(countEvenOdd([1,2,3,4,5,6])); 
+
+// find the second and third largest element in array
+function secondAndThirdLargest(arr){
+    if(arr.length < 3){
+        return null;
+    }
+    let first = -Infinity, second = -Infinity, third = -Infinity;
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] > first){
+            third = second;
+            second = first;
+            first = arr[i];
+        } else if(arr[i] > second && arr[i] !== first){
+            third = second;
+            second = arr[i];
+        } else if(arr[i] > third && arr[i] !== second && arr[i] !== first){
+            third = arr[i];
+        }
+    }
+    return {second, third};
+}
+console.log(secondAndThirdLargest([1,2,3,4,5])); 
