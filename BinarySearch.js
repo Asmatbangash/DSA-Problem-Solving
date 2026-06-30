56de5dfd212c6cd5ad02edfbccda6fa7e67122b7
Asmatbangash <bangashasmat2gmail.com>
2026-06-30 16:45:42 +0500

    implement rotated binary search algorithm

--- BinarySearch.js
+++ BinarySearch.js
@@ -20,3 +20,34 @@ return -1;
 
 
 console.log(binarySearch([1,2,3,4,5,10,20,50],4))
+
+
+// Rotated sorted array 
+function rotatedBinarySearch(arr,target){
+    let st=0;
+    let end=arr.length-1;
+    while(st<=end){
+        let mid=Math.floor(st+(end-st)/2)
+        if(arr[mid]===target){
+            return mid;
+        }
+        // left half is sorted
+        if(arr[st]<=arr[mid]){
+            if(target>=arr[st] && target<arr[mid]){
+                end=mid-1;
+            }else{
+                st=mid+1;
+            }
+        }else{
+            // right half is sorted
+            if(target>arr[mid] && target<=arr[end]){
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+    }
+    return -1;
+}
+
+console.log(rotatedBinarySearch([4,5,6,7,0,1,2],5))
